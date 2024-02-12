fb376f76e0bee3c1ad866ade199ae8023b7f8d11
Олег Кузьмин <gr412_kuoig@lab-57-1.tomtit-tomsk.ru>
2024-02-12 13:28:34 +0700

    Выполнение практической работы №3

--- vue/js/main.js
+++ vue/js/main.js
@@ -4,6 +4,7 @@ let app = new Vue({
         product: "Socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
-        inventory: 100
+        inventory: 100,
+        onSale: true
     }
 })
