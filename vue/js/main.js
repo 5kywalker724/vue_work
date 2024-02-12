d05f1577a34143a69e02de59d8468c2754765076
PinT271 <olezhka.kuzmin.99@inbox.ru>
2024-02-12 16:20:58 +0700

    Выполнение практической работы №5

--- vue/js/main.js
+++ vue/js/main.js
@@ -26,6 +26,9 @@ let app = new Vue({
         },
         updateProduct(variantImage){
             this.image = variantImage;
+        },
+        delFromCart(){
+            this.cart -= 1;
         }
     }
 })
